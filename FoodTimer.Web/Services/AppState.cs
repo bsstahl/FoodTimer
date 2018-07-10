16e7897e59437368f78115678f412c0d8ae53db8
Barry Stahl <barry@bsstahl.com>
2018-07-09 22:47:07 -0700

    Added timer and Reset button. Needs serious styling and ability to work offline

--- FoodTimer.Web/Services/AppState.cs
+++ FoodTimer.Web/Services/AppState.cs
@@ -14,27 +14,58 @@ namespace FoodTimer.Web.Services
         public int CurrentCount { get; private set; }
 
 
+        private System.Timers.Timer _timer;
+
         // Lets components receive change notifications
         // Could have whatever granularity you want (more events, hierarchy...)
         public event Action OnChange;
 
         public AppState()
         {
-            this.StartingTimeSeconds = 900;
+            this.StartingTimeSeconds = 1500;
             this.CurrentCount = this.StartingTimeSeconds;
+
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        public string BackgroundStyle()
+        {
+            string result = "waiting";
+            if (this.IsCounting)
+                if (this.CurrentCount <= 0)
+                    result = "stopWhenSatisfied";
+                else if (this.CurrentCount <= 600)
+                    result = "eatNow";
+                else if (this.CurrentCount <= 900)
+                    result = "pauseEating";
+                else
+                    result = "eatNow";
+
+            return result;
+        }
+
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.CurrentCount--;
+            NotifyStateChanged();
+        }
+
+        public async Task Reset()
+        {
+            this.CurrentCount = this.StartingTimeSeconds;
+            await Task.Run(() => NotifyStateChanged());
         }
 
         public async Task StartStop()
         {
-            // this.IsCounting = !this.IsCounting;
-            Tick();
+            this.IsCounting = !this.IsCounting;
+            if (this.IsCounting)
+                _timer.Start();
+            else
+                _timer.Stop();
             await Task.Run(() => NotifyStateChanged());
-        }
-
-        public void Tick()
-        {
-            this.CurrentCount--;
-            NotifyStateChanged();
         }
 
         private void NotifyStateChanged() => OnChange?.Invoke();
